0154921f151ddc2db6c501d42e856a143d9d6c8a
madhes1986 <76697491+madhes1986@users.noreply.github.com>
2021-08-02 21:45:17 +0530

    Update FegoApplicationTests.java

--- fego_transaction-master/src/test/java/com/ideas2it/fego/FegoApplicationTests.java
+++ fego_transaction-master/src/test/java/com/ideas2it/fego/FegoApplicationTests.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class FegoApplicationTests {
-     public static void main(String[] args) {
-	        	Test.run(Application.class, args);
-     }
+class FegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
 
 }
+
